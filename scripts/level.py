8149422eff5b0674d525193250dc7bab663ed1ab
claudere <129028339+claudere0@users.noreply.github.com>
2026-08-02 17:25:21 +0500

    sprites added and .tmx file updated with adding player in objects layer

--- scripts/level.py
+++ scripts/level.py
@@ -1,14 +1,22 @@
 from .settings import *
+from .sprites import Sprite
 
 class Level:
     def __init__(self, tmx_map):
         self.display_surface = pygame.display.get_surface()
 
+        self.all_sprites = pygame.sprite.Group()
+
         self.setup(tmx_map)
 
     def setup(self, tmx_map):
         for x, y, surf in tmx_map.get_layer_by_name('map').tiles():
-            print(x,y,surf)
+            Sprite((x * TILE_SIZE,y * TILE_SIZE), surf, self.all_sprites)
+
+        for obj in tmx_map.get_layer_by_name('objects'):
+            if obj.name == 'box':
+                print(obj.x, obj.y)
 
     def run(self):
-        self.display_surface.fill(MAGENTA)
+        self.display_surface.fill(BLACK) 
+        self.all_sprites.draw(self.display_surface)
