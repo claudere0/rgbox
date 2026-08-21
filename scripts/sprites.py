8ca09615e7ee7850cbe16379fcf9a46c118f5120
claudere <129028339+claudere0@users.noreply.github.com>
2026-08-21 22:41:30 +0500

    collectible classes added

--- scripts/sprites.py
+++ scripts/sprites.py
@@ -291,3 +291,26 @@ class TextSprite(Sprite):
         self.black_surf = render_text_block((0, 0, 0))
 
         super().__init__(pos, self.white_surf, *groups)
+
+class Collectible(Sprite):
+    def __init__(self, pos, surf, *groups):
+        super().__init__(pos, surf, *groups)
+        self.base_y = self.rect.y
+        self.float_speed = 0.05
+        self.float_amplitude = 16
+
+    def update(self, dt):
+        current_time = pygame.time.get_ticks()
+        self.rect.y = self.base_y + math.sin(current_time * self.float_speed) * self.float_amplitude
+
+class CameoSprite(Collectible):
+    def __init__(self, pos, secret_id, surf, *groups):
+        self.secret_id = secret_id
+        super().__init__(pos, surf, *groups)
+
+class TimeBonusSprite(Collectible):
+    def __init__(self, pos, black_surf, white_surf, bonus_ms, *groups):
+        self.black_surf = black_surf
+        self.white_surf = white_surf
+        self.bonus_ms = bonus_ms
+        super().__init__(pos, self.black_surf, *groups)
